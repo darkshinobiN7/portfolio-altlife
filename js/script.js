51415b1dd2377e2fe008a303b43ff83aeddb9938
darkshinobi <vagnerma@gmail.com>
2022-04-20 10:49:06 +0100

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const responsiveMenu = document.querySelector(".responsive-menu");
+const navMenu = document.querySelector(".nav-menu");
+
+responsiveMenu.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    responsiveMenu.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    responsiveMenu.classList.remove("active");
+    navMenu.classList.remove("active");
+}
